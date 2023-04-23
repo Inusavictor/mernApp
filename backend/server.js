9629d4fdbf3855a8dc06435f39be2bab789db90c
victor <victorinusa@gmail.com>
2023-04-23 09:43:18 +0100

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const goodluck = {
+    complex: 'fair',
+    height: 'short',
+    status: 'married',
+    husband: 'victor'
+}
+
+Object.entries(goodluck).map(([prop, value]) => {
+console.log(`Goodluck has the following ${prop} : ${value}`)
+})
