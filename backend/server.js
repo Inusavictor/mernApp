06a8f6d2c3941acacb7c6431b765728c2569a11b
victor <victorinusa@gmail.com>
2023-04-30 21:16:03 +0100

    deployment ready

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const dotenv = require('dotenv').config()
 const {errorHandler } = require('./middleware/errorMiddleware')
 const Protect = require('./middleware/authMiddleware')
@@ -15,6 +16,14 @@ app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', Protect, require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+//serve frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+    })
+}
 
  app.use(errorHandler)
 
